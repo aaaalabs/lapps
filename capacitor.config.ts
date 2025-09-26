77ce9ce30b9970c5aaddea4e06e5afb666737664
aaaalabs <aaa@libralab.ai>
2025-09-26 11:14:33 +0200

    Initial commit: Fresh Ionic project with Android platform configured for Appflow

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.libra.apps',
+  appName: 'lapps',
+  webDir: 'www'
+};
+
+export default config;
